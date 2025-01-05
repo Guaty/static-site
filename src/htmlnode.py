f6fbd965e105706ca237ab42b1e460bb573e2959
Guaty <bjarocha911@gmail.com>
2025-01-04 22:12:28 -0600

    Edit styling for consistency

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -37,19 +37,19 @@ class LeafNode(HTMLNode):
     
 
 class ParentNode(HTMLNode):
-        def __init__(self, tag, children, props=None):
-            super().__init__(tag, None, children, props)
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
 
-        def to_html(self):
-            if self.tag == None:
-                raise ValueError("ParentNode must have tag")
-            if self.children == None:
-                raise ValueError("ParentNode must have children")
-            html_string = ""
-            for child in self.children:
-                html_string += child.to_html()
-            return f"<{self.tag}{self.props_to_html()}>{html_string}</{self.tag}>"
-        
-        def __repr__(self):
-            return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
-        
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("ParentNode must have tag")
+        if self.children == None:
+            raise ValueError("ParentNode must have children")
+        html_string = ""
+        for child in self.children:
+            html_string += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{html_string}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+    
